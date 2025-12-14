f624be520cf587c6d7f3c8598a0135a8781341b1
Christopher C. Luk <86990758+listingclown3@users.noreply.github.com>
2025-12-14 13:38:54 -0800

    Initialize servo and update laser detection logic

--- laser.ino
+++ laser.ino
@@ -1,17 +1,27 @@
+#include <driver/i2s.h>
+#include <ESP32Servo.h>
+
 const int LASER_PIN = 23;    // Laser Transmitter
 const int SENSOR_PIN = 35;   // Photoresistor (must be an Analog pin like 32, 33, 34, 35)
+const int servoPin = 25;
+
+Servo myServo;
 
 // CHANGE THIS NUMBER based on your calibration readings!
 int THRESHOLD = 2000; 
 
 void setup() {
     Serial.begin(115200);
-    
+    myServo.attach(servoPin, 500, 2400);
+    Serial.println("Initialized Servo");
+    myServo.write(0);
+
     pinMode(LASER_PIN, OUTPUT);
     // GPIO 34 is input-only by hardware design, but defining it is good practice
     pinMode(SENSOR_PIN, INPUT); 
 
     Serial.println("System Initialized.");
+    
     Serial.println("Turn on Laser and check Serial Monitor values.");
 }
 
@@ -29,8 +39,11 @@ void loop() {
     // 4. Check if Laser is hitting the sensor
     if (lightValue > THRESHOLD) {
         Serial.println(" | Status: HIT (Laser Detected)");
+        myServo.write(90); 
+        Serial.println(" | TURNED SERVO");
     } else {
         Serial.println(" | Status: MISS (Beam Broken)");
+        myServo.write(0); 
     }
 
     delay(200); // Small delay to make the text readable
